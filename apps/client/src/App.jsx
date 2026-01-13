ce0ac9c0b4e64e1601a45301282ae8efb8048fbf
heejounglee <heejounglee301@gmail.com>
2026-01-13 12:10:39 +0900

    주석추가

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <SharedLayout title="사용자 사이트">
       {/* 이 부분에 글자를 넣어야 화면에 보입니다 - 주석 by hjlee */}
+      {/*  hjlee 1 */}
       <p style={{ fontSize: '24px', fontWeight: 'bold' }}>test</p>
     </SharedLayout>
   );
