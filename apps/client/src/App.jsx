95d4ee6565fea368af26846f05422918a98bc70d
songl <laina2207@gmail.com>
2026-01-13 13:55:02 +0900

    송아수정

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -3,7 +3,7 @@ import { SharedLayout } from "@gibu/ui";
 function App() {
   return (
     <SharedLayout title="사용자 사이트">
-      {/* 이 부분에 글자를 넣어야 화면에 보입니다 */}
+      {/* 이 부분에 글자를 넣어야 화면에 보입니다 송아수정*/}
       <p style={{ fontSize: '24px', fontWeight: 'bold' }}>test</p>
     </SharedLayout>
   );
