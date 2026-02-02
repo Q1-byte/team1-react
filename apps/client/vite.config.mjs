991e77e4cd58fcbd069de0abd66e9433bea65d25
Q1-byte <gyuwon8440@gmail.com>
2026-02-02 11:58:14 +0900

    프로젝트 이름 gibu에서 trip으로 변경

--- apps/client/vite.config.mjs
+++ apps/client/vite.config.mjs
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // @gibu/ui 임포트 시 실제 물리적 위치로 연결
-      "@gibu/ui": path.resolve(__dirname, "../../packages/ui/index.jsx"),
+      // @trip/ui 임포트 시 실제 물리적 위치로 연결
+      "@trip/ui": path.resolve(__dirname, "../../packages/ui/index.jsx"),
     },
     preserveSymlinks: true
   }
